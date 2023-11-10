0f0adaa2c54631083b1c4cb35514a3b048f1ac43
TakanariShimbo <takanari.shimbo@gmail.com>
2023-11-10 22:43:19 +0900

    revise

--- enums/env_enum.py
+++ enums/env_enum.py
@@ -2,9 +2,8 @@ import os
 from enum import Enum
 
 from dotenv import load_dotenv
+load_dotenv()
 
 
-class EnvEnum(Enum):
-    load_dotenv()
-    
+class EnvEnum(Enum):    
     OPENAI_APIKEY = os.environ["OPENAI_APIKEY"]
