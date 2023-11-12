879838ceb2fda041a35d79668abf0d8a85688aba
TakanariShimbo <takanari.shimbo@gmail.com>
2023-11-12 16:37:15 +0900

    revise

--- pages/3_👀_image_recognition.py
+++ pages/3_👀_image_recognition.py
@@ -13,22 +13,29 @@ TitleComponent.set_page_configs(
     title="Image Recognition",
 )
 
+with st.form(key="Image Recognition Form"):
+    inputed_prompt = st.text_input(label="Prompt")
+    uploaded_image = st.file_uploader(label="Uploader", type=["jpg", "jpeg", "png", "bmp"])
+    is_submit = st.form_submit_button(label="Submit")
 
-upload_image = st.file_uploader(label="Upload Image", type=["jpg", "jpeg", "png", "bmp"])
-if upload_image is not None:
-    with st.spinner(text="Loading..."):
-        image_bytes = upload_image.getvalue()
-        image_b64 = base64.b64encode(s=image_bytes).decode()
-        image_bgr = cv2.imdecode(
-            buf=np.frombuffer(buffer=image_bytes, dtype=np.uint8),
-            flags=cv2.IMREAD_COLOR,
+if is_submit:
+    if not inputed_prompt or not uploaded_image:
+        st.warning("Please fill out the form completely...")
+    else:
+        with st.spinner(text="Image Loading..."):
+            image_bytes = uploaded_image.getvalue()
+            image_b64 = base64.b64encode(s=image_bytes).decode()
+            image_bgr = cv2.imdecode(
+                buf=np.frombuffer(buffer=image_bytes, dtype=np.uint8),
+                flags=cv2.IMREAD_COLOR,
+            )
+            image_rgb = cv2.cvtColor(src=image_bgr, code=cv2.COLOR_BGR2RGB)
+
+        st.image(image=image_rgb, caption=uploaded_image.name, use_column_width=True)
+        
+        answer_area = st.empty()
+        ImageRecognitionHandler.query_and_display_answer_streamly(
+            image_b64=image_b64,
+            prompt=inputed_prompt,
+            display_func=answer_area.write,
         )
-        image_rgb = cv2.cvtColor(src=image_bgr, code=cv2.COLOR_BGR2RGB)
-
-    st.image(image=image_rgb, caption=upload_image.name, use_column_width=True)
-
-    answer_area = st.empty()
-    ImageRecognitionHandler.query_and_display_answer_streamly(
-        image_b64=image_b64,
-        display_func=answer_area.write,
-    )
