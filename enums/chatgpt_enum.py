a5a8db37e7761ba21138963efb25f512978ef5e7
shinbot <shinbot@uniontool.co.jp>
2023-11-23 13:18:22 +0900

    revise

--- enums/chatgpt_enum.py
+++ enums/chatgpt_enum.py
@@ -10,6 +10,7 @@ class AiModelEnum(Enum):
     NONE = None
     GPT4_1106_PREVIEW = "gpt-4-1106-preview"
     GPT4 = "gpt-4"
+    GPT35_TURBO_1106 = "gpt-3.5-turbo-1106"
     GPT35_TURBO = "gpt-3.5-turbo"
     GPT35_TURBO_16K = "gpt-3.5-turbo-16k"
 
