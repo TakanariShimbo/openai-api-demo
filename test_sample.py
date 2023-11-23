42d1a794fff702c9603c70fd00e4e8c3f499ace8
TakanariShimbo <takanari.shimbo@gmail.com>
2023-11-23 18:02:32 +0900

    revise

--- test_sample.py
+++ test_sample.py
@@ -1,8 +1,12 @@
+from openai import OpenAI
+
+from enums.env_enum import EnvEnum
 from enums.chatgpt_enum import AiModelEnum
 from handlers.chatgpt_handler import ChatGptHandler
 
 
 ChatGptHandler.query_answer_and_display_streamly(
+    client=OpenAI(api_key=EnvEnum.DEFAULT_OPENAI_APIKEY.value),
     prompt="hello",
     display_func=print,
     model_type=AiModelEnum.GPT4_1106_PREVIEW,
