2a8c00fb1383568da89bf7545f7be0cc275d1ede
shinbot <shinbot@uniontool.co.jp>
2023-11-23 15:21:22 +0900

    revise

--- components/home_component.py
+++ components/home_component.py
@@ -3,15 +3,22 @@ from textwrap import dedent
 from openai import OpenAI
 import streamlit as st
 
+from enums.env_enum import EnvEnum
+
 
 class HomeComponent:
-    @staticmethod
-    def display_component(client: OpenAI) -> None:
+    @classmethod
+    def display_component(cls, client: OpenAI) -> None:
+        api_key_type = cls.__check_api_key_type(client_api_key=client.api_key)
+
         content = dedent(
             f"""
             #### Overview
             Welcome to demo site of OpenAI API 🤖  
             Let's enjoy some functions 👏  
+
+            #### API Key  
+            Type: {api_key_type}  
 
             #### Creators  
             - Takanari Shimbo 🦥  
@@ -22,3 +29,10 @@ class HomeComponent:
         st.markdown(content)
         st.balloons()
         # st.write(st.session_state)
+
+    @staticmethod
+    def __check_api_key_type(client_api_key: str) -> str:
+        if EnvEnum.DEFAULT_OPENAI_APIKEY.value == client_api_key:
+            return "server's default key"
+        else:
+            return "user's own key"
