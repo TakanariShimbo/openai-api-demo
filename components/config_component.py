5b84f4b55d4d76544f2ba26a063d8974468f93b9
shinbot <shinbot@uniontool.co.jp>
2023-12-04 10:03:20 +0900

    revise

--- components/config_component.py
+++ components/config_component.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 import streamlit as st
 
 
@@ -8,3 +10,15 @@ class ConfigComponent:
             page_title="OpenAI API Demo",
             page_icon="🤖",
         )
+
+
+        # FOR HEADER MENU: 
+        #       [data-testid="stToolbar"] {visibility: hidden !important;}
+        # FOR FOOTER MENU: 
+        #       footer {visibility: hidden !important;}
+        hide_streamlit_style = dedent("""
+            <style>
+            footer {visibility: hidden !important;}
+            </style>
+        """)
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
