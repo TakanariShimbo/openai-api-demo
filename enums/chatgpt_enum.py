989c1e9fdde2124aef4a8709ab7c434df172561c
Shunichi Ikezu <ee26043l@gmail.com>
2024-04-01 09:40:12 +0900

    change models

--- enums/chatgpt_enum.py
+++ enums/chatgpt_enum.py
@@ -8,7 +8,7 @@ class SenderEnum(Enum):
     
 class AiModelEnum(Enum):
     NONE = None
-    GPT4_1106_PREVIEW = "gpt-4-1106-preview"
+    GPT4_TURBO_PREVIEW = "gpt-4-turbo-preview"
     GPT4 = "gpt-4"
     GPT35_TURBO_1106 = "gpt-3.5-turbo-1106"
     GPT35_TURBO = "gpt-3.5-turbo"
